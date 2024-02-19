38ebba0c1e4119727b87e5c05b19bb9ef6df1f24
Joonatan N <Joona-ni@outlook.com>
2024-02-19 11:58:08 +0200

    ThemeSwitcher

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Appbar, PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import MainAppBar from './components/MainAppBar';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+import { ThemeContext } from './context/ThemeContext';
+import Styles from './Styles';
+import ThemeProvider from './context/ThemeProvider';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ThemeProvider>
+          <NavigationContainer>
+            <StatusBar backgroundColor='#666' barStyle="light-content" />
+            <Stack.Navigator
+              initialRouteName='Home'
+              screenOptions={{
+                header: (props) =>
+                  <MainAppBar {...props} backgroundColor='#666' icon='cog' color='#fff' />
+              }}
+            >
+              <Stack.Screen name="Home">
+                {() =>
+                  <Home />
+                }
+              </Stack.Screen>
+              <Stack.Screen name="Settings" component={Settings} />
+            </Stack.Navigator>
+          </NavigationContainer>
+      </ThemeProvider>
+    </PaperProvider>
   );
 }
 
@@ -14,7 +45,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Styles }from "../Styles";
+import { View, Text } from "react-native";
+import { useTheme } from "../context/useTheme";
+
+export default function Home() {
+    const {isDarkMode} = useTheme()
+    return (
+        <View style={[Styles.container, isDarkMode ? Styles.dark : Styles.light]}>
+            <Text style={isDarkMode ? Styles.dark : Styles.light}>Home</Text>
+        </View>
+    )
+}
